892d3bece55cec34ea62b842ea20e22963b8a9ad
JoYoungWook 조영욱 <siosio24@naver.com>
2021-05-21 11:55:19 +0900

    0521 Char print / T : Text / F : Error

---
+++ src/com/company/Day0521/Exam_11.java
@@ -0,0 +1,47 @@
+package com.company.Day0521;
+
+import java.util.Scanner;
+
+public class Exam_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        int max,me ,min;
+
+        if(a>b){
+            if(a>c){
+                max = a;
+                if(b>c){
+                    me=b;
+                    min=c;
+                }else{
+                    me=c;
+                    min =b;
+                }
+            }else{
+              max=c;
+              me=a;
+              min=b;
+            }
+
+        }else if(b>c){
+            max=b;
+            if(a>c){
+                me=a;
+                min=c;
+            }else{
+                me=c;
+                min=a;
+            }
+        }else{
+            max=c;
+            me=b;
+            min=a;
+        }
+    System.out.printf("Max : %d\n Me : %d\n, Min : %d\n", max, me, min);
+    }
+
+}
---
+++ src/com/company/Day0521/Test_04.java
@@ -0,0 +1,11 @@
+package com.company.Day0521;
+/*
+정수를 두개 입력 받아서 큰값과 작은 값을 출력
+같은 값이라면 ""같은값입니다"" 출력
+ */
+
+public class Test_04 {
+    public static void main(String[] args) {
+
+    }
+}
