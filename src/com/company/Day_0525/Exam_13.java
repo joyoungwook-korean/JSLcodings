ea20a3524b9d5123e3081db1a96260f587c382ec
JoYoungWook 조영욱 <siosio24@naver.com>
2021-05-25 11:49:37 +0900

    while 2,8,16 진수 변환

--- src/com/company/Day_0525/Exam_13.java
+++ src/com/company/Day_0525/Exam_13.java
@@ -22,53 +22,41 @@ public class Exam_13 {
     }
 
     public static void cnum2(int num, int zin){
-        int mok;
-        int na;
-        int imsi = num;
         String save ="";
         if(zin==2){ // 2진수 출력 구문
-
-            do {
-
-                mok = imsi/zin;
-                na = imsi%zin;
-
-                imsi=mok;
-
-                save = na + save;
-            }while (!(mok==0));
+            save = gugu(num,zin);
         }else if(zin==8){ // 8진수 출력구문
-            do {
-
-                mok = imsi/zin;
-                na = imsi%zin;
-
-                imsi=mok;
-
-                save = na + save;
-            }while (!(mok==0));
+            save = gugu(num,zin);
         }else if( zin==16){ //  16진수 출력구문
-            do {
-                mok = imsi/zin;
-                na = imsi%zin;
-                imsi=mok;
-                save = na + save;
-            }while (!(mok==0));
-
-            switch (save){
-                case "10": save="A"; break;
-                case "11": save="B"; break;
-                case "12": save="C"; break;
-                case "13": save="D"; break;
-                case "14": save="E"; break;
-                case "15": save="F"; break;
-            }
-            save = na+save;
-
+            save = gugu(num,zin);
         }else{
             System.out.println("진수가 잘못되었습니다");
         }
 
         System.out.println(save);
+    }
+
+    public static String gugu(int num, int zin){
+        int imsi = num;
+        int mok, na;
+        String save="";
+        do {
+            mok = imsi/zin;
+            na = imsi%zin;
+            imsi=mok;
+            save = na+save;
+            if(zin==16){
+                switch (save){
+                    case "10": save="A"; break;
+                    case "11": save="B"; break;
+                    case "12": save="C"; break;
+                    case "13": save="D"; break;
+                    case "14": save="E"; break;
+                    case "15": save="F"; break;
+                }
+            }
+        }while (!(mok==0));
+
+        return  save;
     }
 }
