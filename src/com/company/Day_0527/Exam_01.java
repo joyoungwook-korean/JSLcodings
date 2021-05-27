28d7b1dfac0e39bd8e20eb995e14ab549a8954c9
JoYoungWook 조영욱 <siosio24@naver.com>
2021-05-27 09:38:48 +0900

    0527 구구단 예제

---
+++ src/com/company/Day_0527/Exam_01.java
@@ -0,0 +1,16 @@
+package com.company.Day_0527;
+/*
+구구단
+ */
+public class Exam_01 {
+    public static void main(String[] args) {
+        int cnt=0;
+        for(int i =2; i<=9; i++){
+            for(int j = 2; j<=9; j++){
+                if(cnt%8==0 && !(cnt==0)) System.out.println();
+                System.out.printf("%d * %d = %d ",i,j,i*j);
+                cnt++;
+            }
+        }
+    }
+}
